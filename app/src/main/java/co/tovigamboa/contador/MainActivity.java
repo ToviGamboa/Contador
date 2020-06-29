af6f0902d8f0d90688eb8c4ce26d8faa638e53e8
tovi <contactovictorgamboa@gmail.com>
2020-06-28 22:46:16 -0500

    Boton sumar funcionando

--- app/src/main/java/co/tovigamboa/contador/MainActivity.java
+++ app/src/main/java/co/tovigamboa/contador/MainActivity.java
@@ -1,14 +1,36 @@
 package co.tovigamboa.contador;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TextView tvDisplay;
+	private int contador = 0;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		tvDisplay = findViewById(R.id.tvDisplayId);
+		tvDisplay.setText(String.valueOf(contador));
+	}
+
+	public void onClick(View view) {
+		switch (view.getId()){
+			case R.id.btSumarId:
+				sumar();
+				break;
+		}
+	}
+
+	private void sumar() {
+		contador++;
+		tvDisplay.setText(String.valueOf(contador));
 	}
 }
